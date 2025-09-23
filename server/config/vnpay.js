47619e46795755e37e103b04d7c5e0710e396ad7
KyoTranKMA <quangdieuisme@gmail.com>
2025-09-24 00:14:26 +0700

    chore(payment): handle some format from VN Pay

---
+++ server/config/vnpay.js
@@ -0,0 +1,18 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+const VNPAY_CONFIG = {
+  tmnCode: process.env.VNPAY_TMN_CODE || "",
+  secureSecret: process.env.VNPAY_HASH_SECRET || "",
+  vnpayHost: process.env.VNPAY_HOST || "https://sandbox.vnpayment.vn",
+  paymentEndpoint: process.env.VNPAY_PAYMENT_ENDPOINT || "paymentv2/vpcpay.html",
+  returnUrl: process.env.VNPAY_RETURN_URL || "",
+  ipnUrl:
+    process.env.VNPAY_IPN_URL || `${process.env.SERVER_URL}/api/payment/vnpay/ipn`,
+  locale: process.env.VNPAY_LOCALE || "vn",
+  currCode: "VND",
+};
+
+export default VNPAY_CONFIG;
+
+
