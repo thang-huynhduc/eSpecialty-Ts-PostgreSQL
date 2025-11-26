128cb30716a734b2ef0135f030f459414825f45c
Thang Huynh Duc <thang.huynhduc.work@gmail.com>
2025-11-26 19:48:09 +0700

    ref: init prisma client and test

---
+++ ts-server/src/test-script.ts
@@ -0,0 +1,76 @@
+import { prisma } from './config/prisma.js'
+
+async function main() {
+  // 1. CREATE USER (có address + contact + otp)
+  const user = await prisma.user.create({
+    data: {
+      name: 'Alice',
+      email: 'alice@example.com',
+      password: 'hashed-password-123',
+      role: 'USER',
+
+      addresses: {
+        create: [
+          {
+            label: 'Home',
+            street: '123 Main St',
+            city: 'HCM',
+            district: '1',
+            phone: '0901234567',
+            isDefault: true
+          }
+        ],
+      },
+
+      contacts: {
+        create: [
+          {
+            name: 'Alice',
+            email: 'alice@example.com',
+            subject: 'Test Contact',
+            message: 'Hello admin!'
+          }
+        ]
+      },
+
+      otps: {
+        create: [
+          {
+            otpHash: 'hashed-otp-123',
+            type: 'register',
+            expiresAt: new Date(Date.now() + 1000 * 60 * 5) // +5 phút
+          }
+        ]
+      }
+    },
+    include: {
+      addresses: true,
+      contacts: true,
+      otps: true,
+    },
+  })
+
+  console.log('Created user:', JSON.stringify(user, null, 2))
+
+  // 2. GET ALL USERS (kèm quan hệ)
+  const allUsers = await prisma.user.findMany({
+    include: {
+      addresses: true,
+      contacts: true,
+      otps: true,
+      orders: true
+    },
+  })
+
+  console.log('All users:', JSON.stringify(allUsers, null, 2))
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
